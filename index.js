d092680dd41afa14182bfa57ddae2187199301b0
Dave Honneffer <pearofducks@gmail.com>
2025-03-07 09:20:52 +0100

    Cleanup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { handleArgs } from './util.js'
 
-export function h(tag, attrsOrChildren, maybeChildren, opts) {
+/** @type {import('./types/index.d.ts').h} */
+export function h(tag) {
   const { attrs, children, isVoid } = handleArgs(arguments)
   let html = `<${tag}${attrs}${isVoid ? '/' : ''}>`
   if (!isVoid) html += `${children}</${tag}>`
